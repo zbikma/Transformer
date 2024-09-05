72acc2b86b12823db70aac0782648c7419ecc1dd
zbikma <z.mazadi@gmail.com>
2024-09-05 18:02:12 -0400

    commit up to here but there is an issue with the deployment

--- scripts/ParallelDistributedComputation/matrixmultiply.py
+++ scripts/ParallelDistributedComputation/matrixmultiply.py
@@ -8,10 +8,17 @@ def multiplymatrix(matrix_a,matrix_b):
 
 if __name__ == "__main__":
     matrix_size=1000
+    output_file_path = "/data/matrix_result.txt"
     print(f"generating two {matrix_size} x {matrix_size} matrices...")
     matrix_a,matrix_b= generateLargeMatrix(matrix_size),generateLargeMatrix(matrix_size)
     print("multiplying the matrices...")
     result = multiplymatrix(matrix_a=matrix_a,matrix_b=matrix_b)
     print(f"multiplication of the two matrix has the shape of {result.shape}")
+    
+    print(f"saving result in {output_file_path}...")
+    with open(output_file_path,"w") as f:
+        np.savetxt(f,result)
+    print(f"Matrix multiplication result saved to {output_file_path}")
+        
     
     
